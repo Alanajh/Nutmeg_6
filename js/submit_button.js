d33eb29eca0e78fdbf94e6764ad588650180facd
Harmon <alana.harmon@yahoo.com>
2021-06-05 16:50:56 -0500

    Adding first batch of files

---
+++ js/submit_button.js
@@ -0,0 +1,19 @@
+function submit_button(){
+    if(document.getElementById("testTitle").style.color != "red"){
+        document.getElementById("testTitle").style.color = "red";  
+    } else {
+        document.getElementById("testTitle").style.color = "green";
+    }
+}
+
+function getTestTitles() {
+
+}
+
+function getTestQuestions() {
+
+}
+
+function getTestAnswers() {
+    
+}
